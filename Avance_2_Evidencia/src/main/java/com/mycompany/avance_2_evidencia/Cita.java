bba7d5a7b2a0c32107700a4ad85410916d2880b6
LDGAFer <90427903+LDGAFer@users.noreply.github.com>
2022-07-01 06:26:48 -0500

    Add files via upload

---
+++ Avance_2_Evidencia/src/main/java/com/mycompany/avance_2_evidencia/Cita.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.avance_2_evidencia;
+
+import java.util.Date;
+
+/**
+ *
+ * @author USER
+ */
+public class Cita {
+    
+    private Integer id;
+    private String nombreCita;
+    private Date fecha;
+    private Medico medico;
+    private Paciente paciente;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombreCita() {
+        return nombreCita;
+    }
+
+    public void setNombreCita(String nombreCita) {
+        this.nombreCita = nombreCita;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Medico getMedico() {
+        return medico;
+    }
+
+    public void setMedico(Medico medico) {
+        this.medico = medico;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+
+}
---
+++ Avance_2_Evidencia/src/main/java/com/mycompany/avance_2_evidencia/Medico.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.avance_2_evidencia;
+
+/**
+ *
+ * @author USER
+ */
+public class Medico {
+       private Integer id;
+    private String nombre;
+    private String especialida;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEspecialida() {
+        return especialida;
+    }
+
+    public void setEspecialida(String especialida) {
+        this.especialida = especialida;
+    }
+ 
+}
